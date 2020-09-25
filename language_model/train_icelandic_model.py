e8200192c9d31890e00ce81f84fac2dd4a13daa0
Bobby Donchev <contact@donchev.is>
2020-09-25 17:59:41 +0200

    Icelandic language model

---
+++ language_model/train_icelandic_model.py
@@ -0,0 +1,51 @@
+import tokenizers
+from transformers import RobertaConfig
+from transformers import RobertaTokenizerFast
+from transformers import RobertaForMaskedLM
+from transformers import LineByLineTextDataset
+from transformers import DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+config = RobertaConfig(
+    vocab_size=52_000,
+    max_position_embeddings=514,
+    num_attention_heads=12,
+    num_hidden_layers=6,
+    type_vocab_size=1,
+)
+
+tokenizers = RobertaTokenizerFast.from_pretrained("./icelandic", max_len=512)
+model = RobertaForMaskedLM(config=config)
+
+print(model.num_parameters())
+
+dataset = LineByLineTextDataset(
+    tokenizer=tokenizers,
+    file_path="./data/is.txt",
+    block_size=128,
+)
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizers, mlm=True, mlm_probability=0.15
+)
+
+training_args = TrainingArguments(
+    output_dir="./icelandic",
+    overwrite_output_dir=True,
+    num_train_epochs=1,
+    per_gpu_train_batch_size=64,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=dataset,
+    prediction_loss_only=True,
+)
+
+
+trainer.train()
+trainer.save_model("./icelandic")
---
+++ language_model/train_tokenizer.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from tokenizers import ByteLevelBPETokenizer
+
+paths = [str(x) for x in Path("./data/").glob("*.txt")]
+tokenizer = ByteLevelBPETokenizer()
+
+tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
+    "<s>",
+    "<pad>",
+    "</s>",
+    "<unk>",
+    "<mask>",
+])
+
+# Save files to disk
+tokenizer.save_model("./icelandic")
+
+
+
