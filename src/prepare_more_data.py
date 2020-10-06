24e160ded77fc801712a4cbc446f89057303e070
elenacramer <eleanascramer@gmail.com>
2020-10-06 10:49:34 +0200

    'getting new data'

--- src/prepare_more_data.py
+++ src/prepare_more_data.py
@@ -5,12 +5,25 @@ import zipfile
 os.chdir("/home/elena/Projects/neurocode/icelandic-language-model/src")
 print(os.getcwd())
 
+# def get_text_file(tname):
+#     with tarfile.open(tname, "r:gz") as tf:
+#         for member in tf.getmembers():
+#             if member.name != "isl_newscrawl_2019_300K-sentences.txt":
+#                 continue
+#             file_wanted = tf.extractall(path = "./data/", members =[member])
+#             out_f=open("./data/new_data.txt", "w")
+#             out_f.write(file_wanted.read())
+#             out_f.close()
+          
+
+# get_text_file("./data/isl_newscrawl_2019_300K.tar.gz")
+
 def extract_tar_files(tname):
     with tarfile.open(tname, "r:gz") as tf:
         tf.extractall("./data/")
 
 
-# extract_tar_files("./data/isl_newscrawl_2019_300K.tar.gz")
+extract_tar_files("./data/isl_newscrawl_2019_300K.tar.gz")
 
 def open_file(fname):
     in_f = open(fname, "r")
